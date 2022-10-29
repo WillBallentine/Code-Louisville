5ffe2b85340245d4994199a2bd6b66eba44ef72f
Will Ballentine <willballentine@wills-mbp.lan>
2022-10-29 04:39:52 -0400

    current date

--- datasort.py
+++ datasort.py
@@ -1,6 +1,11 @@
 import csv
 import requests
 import json
+from datetime import date
+
+today = date.today()
+start_date = ('start_date=' + str(today))
+end_date = ('end_date=' + str(today))
 
 def temphigh():
     file1 = json.loads(open('myData.json').read())
@@ -34,7 +39,7 @@ def main():
 
     with open('myData.json', 'w') as f:
         count = 0
-        url = "https://api.open-meteo.com/v1/forecast?latitude=38.2527&longitude=85.7585&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&start_date=2022-10-29&end_date=2022-10-29"
+        url = ("https://api.open-meteo.com/v1/forecast?latitude=38.2527&longitude=85.7585&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&" + start_date + "&" + end_date)
         response = requests.request("GET", url)
         today = str(response.json())
         today2 = today.replace("\'", "\"")
@@ -46,7 +51,7 @@ def main():
         count = count + 1
     #print(count)
     f.close()
-    print("1. High Temp \n 2. Low Temp \n 3. sunrise \n 4. sunset")
+    print("1. High Temp \n 2. Low Temp \n 3. Sunrise \n 4. Sunset \n 5. Quit")
     userinput = input("What weather data would you like?")
 
     if userinput == "1":
@@ -57,6 +62,8 @@ def main():
         sunrise()
     elif userinput == "4":
         sunset()
+    elif userinput == "5":
+        exit()
     elif print('not an option, try again'):
         main()
 
